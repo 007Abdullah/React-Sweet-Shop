dbf9d91cd19ff1a8d266c122b52f8cf4c8740833
faz.pak@gmail.com <kb337137@gmail.com>
2021-02-18 17:19:51 +0500

    signup done

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login'
+import Signup from './components/Signup'
+import Home from './components/Home'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/Login">Login</Link>
+              </li>
+              <li>
+                <Link to="/Signup">SignUp</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/Login">
+              <Login />
+            </Route>
+            <Route path="/Signup">
+              <Signup />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </>
   );
 }
 
